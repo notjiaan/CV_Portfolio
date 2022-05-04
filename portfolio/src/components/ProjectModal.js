d648e3e4dbcfc63338abdc55d05932f792685da7
jiaanabad <jiaanpatrick.abad.iics@ust.edu.ph>
2022-05-04 11:50:00 +0800

    final output 3

--- portfolio/src/components/ProjectModal.js
+++ portfolio/src/components/ProjectModal.js
@@ -1,27 +1,33 @@
-import React from 'react';
-import { Modal } from "react-bootstrap";
+import React from "react";
+import { Modal, Container } from "react-bootstrap";
 import "../css/comp-style/comp-style.css";
-
 
 const ProjectModal = (props) => {
   return (
-    <Modal size="xl" show={props.show} onHide={props.modalVisible} className="project-modal">
-        <Modal.Header closeButton closeVariant='white'>
-          <Modal.Title>{props.title}</Modal.Title>
-        </Modal.Header>
+    <Modal
+      size="xl"
+      show={props.show}
+      onHide={props.modalVisible}
+      className="project-modal"
+    >
+      <Modal.Header closeButton closeVariant="white">
+        <Modal.Title className="project-modal-title">{props.title}</Modal.Title>
+      </Modal.Header>
+      <Container>
         <Modal.Body>
-          <p>
+          <p className="project-modal-desrciption">
             {props.description}
             <br />
           </p>
-          <p>{props.year}</p>
-          <p> {props.role}</p>
-          <p> {props.type}</p>
-          <p>{props.technologies}</p>
+          <p className="project-modal-desrciption">{props.year}</p>
+          <p className="project-modal-desrciption"> {props.role}</p>
+          <p className="project-modal-desrciption"> {props.type}</p>
+          <p className="project-modal-desrciption">{props.technologies}</p>
           <hr />
         </Modal.Body>
-      </Modal>
-  )
-}
+      </Container>
+    </Modal>
+  );
+};
 
-export default ProjectModal
+export default ProjectModal;
